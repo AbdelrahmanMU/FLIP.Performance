a9ad64ea53253153f6c12819005fb59b3c6ee456
abdelrahman mohamed <abdo.hamadah.7050@gmail.com>
2025-04-27 16:27:59 +0300

    Remoce unused files and methods

--- FLIP.Application/Helpers/APIHelper.cs
+++ FLIP.Application/Helpers/APIHelper.cs
@@ -15,37 +15,11 @@ public static class APIHelper
 
         var options = new JsonSerializerOptions
         {
-            PropertyNameCaseInsensitive = true // Enable case-insensitive deserialization
+            PropertyNameCaseInsensitive = true
         };
 
         var requests = JsonSerializer.Deserialize<List<ApiRequest>>(json, options);
 
         return requests ?? [];
     }
-
-    public static List<FreelancerData> DumyData()
-    {
-        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "DummyData.json");
-
-        string json = File.ReadAllText(filePath);
-
-        using JsonDocument document = JsonDocument.Parse(json);
-        var freelancers = new List<FreelancerData>();
-
-        foreach (var element in document.RootElement.EnumerateArray())
-        {
-            var freelancer = new FreelancerData
-            {
-                TransactionID = element.GetProperty("TransactionID").GetGuid(),
-                PlatformName = element.GetProperty("PlatformName").GetString()!,
-                NationalId = element.GetProperty("NationalId").GetInt64().ToString(), // Keeping as string
-                JsonContent = element.GetProperty("JsonContent").GetRawText() // Serialize JsonContent as a string
-            };
-
-            freelancers.Add(freelancer);
-        }
-
-        return freelancers ?? [];
-    }
-
 }
