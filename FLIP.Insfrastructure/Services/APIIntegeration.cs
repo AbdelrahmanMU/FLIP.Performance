fab726f7801eb63df4c366ef6760d423b1d70171
abdelrahman mohamed <abdo.hamadah.7050@gmail.com>
2025-08-08 23:23:33 +0300

    Fix

--- FLIP.Insfrastructure/Services/APIIntegeration.cs
+++ FLIP.Insfrastructure/Services/APIIntegeration.cs
@@ -14,232 +14,232 @@ using System.Net.Http.Headers;
 namespace FLIP.Infrastructure.Services;
 
 public class APIIntegeration(IConfiguration configuration,
-    IDapperQueries dapperQueries,
-    IMemoryCache memoryCache) : IAPIIntegeration
+	IDapperQueries dapperQueries,
+	IMemoryCache memoryCache) : IAPIIntegeration
 {
-    private readonly HttpClient _httpClient = new();
-    private readonly int _maxDegreeOfParallelism = 16;
-    private readonly ILogger _logger = Log.Logger;
-    private ApiLog _log = new();
-    private ErrorLogs _errorLog = new();
-    private FreelancerData _freelancersData = new();
-    private readonly IConfiguration _configuration = configuration;
-    private readonly IDapperQueries _dapperQueries = dapperQueries;
-    private readonly IMemoryCache _memoryCache = memoryCache;
+	private readonly HttpClient _httpClient = new();
+	private readonly int _maxDegreeOfParallelism = 16;
+	private readonly ILogger _logger = Log.Logger;
+	private ApiLog _log = new();
+	private ErrorLogs _errorLog = new();
+	private FreelancerData _freelancersData = new();
+	private readonly IConfiguration _configuration = configuration;
+	private readonly IDapperQueries _dapperQueries = dapperQueries;
+	private readonly IMemoryCache _memoryCache = memoryCache;
 
-    public async Task<Response> ProcessId(FreelancerDto freelancer)
-    {
-        var stopwatch = Stopwatch.StartNew();
+	public async Task<Response> ProcessId(FreelancerDto freelancer)
+	{
+		var stopwatch = Stopwatch.StartNew();
 
-        //if (_memoryCache.TryGetValue(freelancer.Id, out _))
-        //{
-        //    // ID is already cached
-        //    return new Response
-        //    {
-        //        Success = true,
-        //        StatusCode = (int)HttpStatusCode.OK,
-        //    };
-        //}
+		//if (_memoryCache.TryGetValue(freelancer.Id, out _))
+		//{
+		//    // ID is already cached
+		//    return new Response
+		//    {
+		//        Success = true,
+		//        StatusCode = (int)HttpStatusCode.OK,
+		//    };
+		//}
 
-        //// ID is not cached, so we add it
-        //_memoryCache.Set(freelancer.Id, true);
+		//// ID is not cached, so we add it
+		//_memoryCache.Set(freelancer.Id, true);
 
-        // At least one success
-        var (success, apiLogs, freelancerDataResponse, errorLogs) = await ExecuteParallelApiCallsAsync(freelancer);
-        if (success)
-        {
-            try
-            {
-                if (freelancer.IsUpdating)
-                {
-                    return new Response
-                    {
-                        Success = true,
-                        StatusCode = (int)HttpStatusCode.OK,
-                        FreelancerData = freelancerDataResponse,
-                        ApiLogData = apiLogs,
-                        ErrorLogsData = errorLogs
-                    };
-                }
+		// At least one success
+		var (success, apiLogs, freelancerDataResponse, errorLogs) = await ExecuteParallelApiCallsAsync(freelancer);
+		if (success)
+		{
+			try
+			{
+				if (freelancer.IsUpdating)
+				{
+					return new Response
+					{
+						Success = true,
+						StatusCode = (int)HttpStatusCode.OK,
+						FreelancerData = freelancerDataResponse,
+						ApiLogData = apiLogs,
+						ErrorLogsData = errorLogs
+					};
+				}
 
-                var rides = freelancerDataResponse.IsRide ? freelancerDataResponse : new FreelancerData();
+				var rides = freelancerDataResponse.IsRide ? freelancerDataResponse : null;
 
-                var projects = !freelancerDataResponse.IsRide ? freelancerDataResponse : new FreelancerData();
+				var projects = !freelancerDataResponse.IsRide ? freelancerDataResponse : null;
 
-                if (rides is not null)
-                {
-                    await _dapperQueries.InsertFreelancerRide(rides);
-                }
+				if (rides is not null)
+				{
+					await _dapperQueries.InsertFreelancerRide(rides);
+				}
 
-                if (projects is not null)
-                {
-                    await _dapperQueries.InsertFreelancers(projects);
-                }
+				if (projects is not null)
+				{
+					await _dapperQueries.InsertFreelancers(projects);
+				}
 
-                await _dapperQueries.InsertLogs(apiLogs);
-                await _dapperQueries.InsertErrorLogs(errorLogs);
+				await _dapperQueries.InsertLogs(apiLogs);
+				await _dapperQueries.InsertErrorLogs(errorLogs);
 
-                stopwatch.Stop();
+				stopwatch.Stop();
 
-                return new Response
-                {
-                    Success = true,
-                    StatusCode = (int)HttpStatusCode.OK,
-                    FreelancerData = freelancerDataResponse,
-                    ApiLogData = apiLogs,
-                    ErrorLogsData = errorLogs
-                };
-            }
-            catch (Exception ex)
-            {
-                stopwatch.Stop();
-                _logger.Error("Failed to update the DB:", ex.Message);
-                throw new Exception(ex.Message);
-            }
-        }
-        else
-        {
-            _logger.Error("Failed to ExecuteParallelApiCallsAsync");
+				return new Response
+				{
+					Success = true,
+					StatusCode = (int)HttpStatusCode.OK,
+					FreelancerData = freelancerDataResponse,
+					ApiLogData = apiLogs,
+					ErrorLogsData = errorLogs
+				};
+			}
+			catch (Exception ex)
+			{
+				stopwatch.Stop();
+				_logger.Error("Failed to update the DB:", ex.Message);
+				throw new Exception(ex.Message);
+			}
+		}
+		else
+		{
+			_logger.Error("Failed to ExecuteParallelApiCallsAsync");
 
-            stopwatch.Stop();
+			stopwatch.Stop();
 
-            return new Response
-            {
-                Success = false,
-                StatusCode = (int)HttpStatusCode.BadRequest,
-                Message = "There is no any API get successed",
-                Errors = apiLogs.Message
-            };
-        }
-    }
+			return new Response
+			{
+				Success = false,
+				StatusCode = (int)HttpStatusCode.BadRequest,
+				Message = "There is no any API get successed",
+				Errors = apiLogs.Message
+			};
+		}
+	}
 
-    protected virtual async Task<(bool, ApiLog, FreelancerData, ErrorLogs)> ExecuteParallelApiCallsAsync(FreelancerDto freelancerDto)
-    {
-        var tasks = new List<Task<bool>>();
+	protected virtual async Task<(bool, ApiLog, FreelancerData, ErrorLogs)> ExecuteParallelApiCallsAsync(FreelancerDto freelancerDto)
+	{
+		var tasks = new List<Task<bool>>();
 
-        var apis = APIHelper.APIRequests();
+		var apis = APIHelper.APIRequests();
 
-        using (var semaphore = new SemaphoreSlim(_maxDegreeOfParallelism))
-        {
-            var totalStopwatch = Stopwatch.StartNew(); // Start benchmark
+		using (var semaphore = new SemaphoreSlim(_maxDegreeOfParallelism))
+		{
+			var totalStopwatch = Stopwatch.StartNew(); // Start benchmark
 
-            freelancerDto.Api.Params.Insert(0, freelancerDto.Id);
+			freelancerDto.Api.Params.Insert(0, freelancerDto.Id);
 
-            freelancerDto.Api.PrepareParams();
+			freelancerDto.Api.PrepareParams();
 
-            await semaphore.WaitAsync(); // Control concurrency level
+			await semaphore.WaitAsync(); // Control concurrency level
 
-            tasks.Add(Task.Run(async () =>
-            {
-                try
-                {
-                    return await CallExternalApiAsync(freelancerDto.Api, freelancerDto.Id);
-                }
-                finally
-                {
-                    semaphore.Release();
-                }
-            }));
+			tasks.Add(Task.Run(async () =>
+			{
+				try
+				{
+					return await CallExternalApiAsync(freelancerDto.Api, freelancerDto.Id);
+				}
+				finally
+				{
+					semaphore.Release();
+				}
+			}));
 
-            totalStopwatch.Stop();
+			totalStopwatch.Stop();
 
-            double totalExecutionTimeMs = totalStopwatch.Elapsed.TotalMilliseconds;
+			double totalExecutionTimeMs = totalStopwatch.Elapsed.TotalMilliseconds;
 
-            _logger.Information($"Total response time is: {totalExecutionTimeMs}");
+			_logger.Information($"Total response time is: {totalExecutionTimeMs}");
 
-            await Task.WhenAll(tasks);
-        }
+			await Task.WhenAll(tasks);
+		}
 
-        int successCount = tasks.Count(t => t.Result);
-        _logger.Information($"Completed {tasks.Count} API calls, {successCount} were successful.");
+		int successCount = tasks.Count(t => t.Result);
+		_logger.Information($"Completed {tasks.Count} API calls, {successCount} were successful.");
 
-        return (successCount >= 1, _log, _freelancersData, _errorLog);
-    }
+		return (successCount >= 1, _log, _freelancersData, _errorLog);
+	}
 
-    private async Task<bool> CallExternalApiAsync(ApiRequest api, string id)
-    {
-        var retryCount = int.Parse(_configuration["retryCount"] ?? "");
-        var maxResponseTime = int.Parse(_configuration["maxResponseTime"] ?? "");
+	private async Task<bool> CallExternalApiAsync(ApiRequest api, string id)
+	{
+		var retryCount = int.Parse(_configuration["retryCount"] ?? "");
+		var maxResponseTime = int.Parse(_configuration["maxResponseTime"] ?? "");
 
-        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(maxResponseTime));
-        var cancellationToken = cts.Token;
+		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(maxResponseTime));
+		var cancellationToken = cts.Token;
 
-        var policy = Policy
-            .Handle<HttpRequestException>()
-            .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode && r.StatusCode != HttpStatusCode.NotFound)
-            .Or<TimeoutRejectedException>()
-            .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+		var policy = Policy
+			.Handle<HttpRequestException>()
+			.OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode && r.StatusCode != HttpStatusCode.NotFound)
+			.Or<TimeoutRejectedException>()
+			.WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
-        HttpResponseMessage response = new();
-        var log = new ApiLog();
-        var freelancerData = new FreelancerData();
-        var stopwatch = Stopwatch.StartNew(); // Track API response time
+		HttpResponseMessage response = new();
+		var log = new ApiLog();
+		var freelancerData = new FreelancerData();
+		var stopwatch = Stopwatch.StartNew(); // Track API response time
 
-        try
-        {
-            if (api.IsBearerAuth)
-            {
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", api.BearerToken);
-            }
+		try
+		{
+			if (api.IsBearerAuth)
+			{
+				_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", api.BearerToken);
+			}
 
-            response = await policy.ExecuteAsync(async (ct) =>
-            {
-                // Perform the HTTP GET request with the cancellation token
-                return await _httpClient.GetAsync($"{api.Url}", ct);
-            }, cancellationToken);
+			response = await policy.ExecuteAsync(async (ct) =>
+			{
+				// Perform the HTTP GET request with the cancellation token
+				return await _httpClient.GetAsync($"{api.Url}", ct);
+			}, cancellationToken);
 
-            stopwatch.Stop();
-            double responseTimeMs = stopwatch.Elapsed.TotalMilliseconds;
+			stopwatch.Stop();
+			double responseTimeMs = stopwatch.Elapsed.TotalMilliseconds;
 
-            log = new ApiLog
-            {
-                RequestUri = api.Url,
-                StatusCode = (int)response.StatusCode,
-                LoggedAt = DateTime.Now,
-                Message = response.ReasonPhrase,
-                ResponseTimeMs = responseTimeMs
-            };
+			log = new ApiLog
+			{
+				RequestUri = api.Url,
+				StatusCode = (int)response.StatusCode,
+				LoggedAt = DateTime.Now,
+				Message = response.ReasonPhrase,
+				ResponseTimeMs = responseTimeMs
+			};
 
-            if (response.IsSuccessStatusCode)
-            {
-                freelancerData = new FreelancerData
-                {
-                    TransactionID = Guid.NewGuid(),
-                    PlatformName = api.PlatformName,
-                    IngestedAt = DateTime.Now.AddDays(-1),
-                    NationalId = id,
-                    JsonContent = await response.Content.ReadAsStringAsync(),
-                    IsRide = api.IsRide
-                };
+			if (response.IsSuccessStatusCode)
+			{
+				freelancerData = new FreelancerData
+				{
+					TransactionID = Guid.NewGuid(),
+					PlatformName = api.PlatformName,
+					IngestedAt = DateTime.Now.AddDays(-1),
+					NationalId = id,
+					JsonContent = await response.Content.ReadAsStringAsync(),
+					IsRide = api.IsRide
+				};
 
-                _freelancersData = freelancerData;
-            }
+				_freelancersData = freelancerData;
+			}
 
-            _log = log;
+			_log = log;
 
-            return response.IsSuccessStatusCode;
-        }
-        catch (Exception ex)
-        {
-            stopwatch.Stop();
-            _logger.Error($"[API Caller] API call failed after {retryCount} retries", ex.Message);
-           
-            log.Message = ex.Message;
-            log.RequestUri = api.Url;
-            log.StatusCode = (int)response.StatusCode;
+			return response.IsSuccessStatusCode;
+		}
+		catch (Exception ex)
+		{
+			stopwatch.Stop();
+			_logger.Error($"[API Caller] API call failed after {retryCount} retries", ex.Message);
 
-            var errorLog = new ErrorLogs
-            {
-                RequestUrl = api.Url,
-                RequestPayload = id.ToString(),
-                ErrorMessage = ex.Message,
-                LoggedAt = DateTime.UtcNow
-            };
+			log.Message = ex.Message;
+			log.RequestUri = api.Url;
+			log.StatusCode = (int)response.StatusCode;
 
-            _log = log;
-            _errorLog = errorLog;
+			var errorLog = new ErrorLogs
+			{
+				RequestUrl = api.Url,
+				RequestPayload = id.ToString(),
+				ErrorMessage = ex.Message,
+				LoggedAt = DateTime.UtcNow
+			};
 
-            throw new Exception(ex.Message);
-        }
-    }
+			_log = log;
+			_errorLog = errorLog;
+
+			throw new Exception(ex.Message);
+		}
+	}
 }
